0b1e6dd53eb3cf5ac63877850482e9c9ca40444a
lin <2387259434@qq.com>
2023-03-28 19:38:50 +0800

    爬取10页

--- 03 豆瓣.py
+++ 03 豆瓣.py
@@ -7,15 +7,37 @@ import urllib.request
 import urllib.parse
 
 
+def create_request(page):
+    base_url = 'https://movie.douban.com/j/chart/top_list?type=25&interval_id=100%3A90&action=&'
+    data = {
+        'start': (page - 1) * 20,
+        'limit': 20
+    }
+    data = urllib.parse.urlencode(data)
+    url = base_url + data
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+    }
+    request = urllib.request.Request(url=url, headers=header)
+    return request
 
-url = 'https://movie.douban.com/j/chart/top_list?type=25&interval_id=100%3A90&action=&start=0&limit=20'
 
-header = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-}
-request = urllib.request.Request(url=url, headers=header)
-response = urllib.request.urlopen(request)
-content = response.read().decode('utf-8')
+def get_content(request):
+    response = urllib.request.urlopen(request)
+    content = response.read().decode('utf-8')
+    return content
 
-with open('download/douban.json', 'w', encoding='utf-8') as file:
-    file.write(content)
+
+def download(page, content):
+    with open('download/douban' + str(page) + '.json', 'w', encoding='utf-8') as file:
+        file.write(content)
+
+
+if __name__ == '__main__':
+    startPage = int(input('请输入起始页'))
+    endPage = int(input('请输入结束页'))
+
+    for page in range(startPage, endPage + 1):
+        request = create_request(page)
+        content = get_content(request)
+        download(page, content)
